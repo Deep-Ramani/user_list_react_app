da39c1a4a94e3229f19e9a2f81be7bcb4fa073b7
Deep-Ramani <deepramani8505@gmail.com>
2022-03-07 15:17:28 +0530

    feat:Added UserList Component in Main Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
-import './App.css';
+import './App.css'; 
+import UserList from './Component/UserList/UserList';
 
 function App() {
   return (
     <div className="App">
-  
+      <UserList/>
     </div>
   );
 }
