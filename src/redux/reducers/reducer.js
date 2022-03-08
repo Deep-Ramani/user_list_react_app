499f667828de17a759f5dfe783b9e3c1a22a9865
Deep-Ramani <deepramani8505@gmail.com>
2022-03-08 14:41:30 +0530

    feat:Add Status Dummy data in reducer

--- src/redux/reducers/reducer.js
+++ src/redux/reducers/reducer.js
@@ -3,57 +3,68 @@ const intialState = [
         Name : "Deep Ramani",
         Image : 'https://reqres.in/img/faces/1-image.jpg',
         Email : 'deepramani@gmail.com',
+        Status : "Active"
 
     },
     {
         Name : "Deep Ramani",
         Image : 'https://reqres.in/img/faces/2-image.jpg',
         Email : 'deepramani@gmail.com',
+        Status : "Inactive"
     },
     {
         Name : "Deep Ramani",
         Image : 'https://reqres.in/img/faces/3-image.jpg',
         Email : 'deepramani@gmail.com',
+        Status : "Inactive"
     },
     {
         Name : "Deep Ramani",
         Image : 'https://reqres.in/img/faces/4-image.jpg',
         Email : 'deepramani@gmail.com',
+        Status : "Inactive"
     },
     {
         Name : "Deep Ramani",
         Image : 'https://reqres.in/img/faces/5-image.jpg',
         Email : 'deepramani@gmail.com',
+        Status : "Inactive"
     },
     {
         Name : "Deep Ramani",
         Image : 'https://reqres.in/img/faces/6-image.jpg',
         Email : 'deepramani@gmail.com',
+        Status : "Inactive"
     },
     {
         Name : "Deep Ramani",
         Image : 'https://reqres.in/img/faces/7-image.jpg',
         Email : 'deepramani@gmail.com',
+        Status : "Inactive"
     },
     {
         Name : "Deep Ramani",
         Image : 'https://reqres.in/img/faces/8-image.jpg',
         Email : 'deepramani@gmail.com',
+        Status : "Inactive"
     },
     {
         Name : "Deep Ramani",
         Image : 'https://reqres.in/img/faces/9-image.jpg',
         Email : 'deepramani@gmail.com',
+        Status : "Inactive"
     },
     {
         Name : "Deep Ramani",
         Image : 'https://reqres.in/img/faces/10-image.jpg',
         Email : 'deepramani@gmail.com',
+        Status : "Inactive"
     },
     {
         Name : "Deep Ramani",
         Image : 'https://reqres.in/img/faces/11-image.jpg',
         Email : 'deepramani@gmail.com',
+        Status : "Inactive"
     },
   
 ];
