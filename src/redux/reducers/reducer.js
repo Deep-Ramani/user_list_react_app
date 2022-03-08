922e22c58b9b5e25dc618bd8938ce242cf3863a5
Deep-Ramani <deepramani8505@gmail.com>
2022-03-08 15:03:07 +0530

    feat:Store dummy data in reducer for access component

--- src/redux/reducers/reducer.js
+++ src/redux/reducers/reducer.js
@@ -1,70 +1,83 @@
+
+
 const intialState = [
     {
         Name : "Deep Ramani",
         Image : 'https://reqres.in/img/faces/1-image.jpg',
         Email : 'deepramani@gmail.com',
-        Status : "Active"
+        Status : "Active",
+        Access : "Owner"
 
     },
     {
         Name : "Deep Ramani",
         Image : 'https://reqres.in/img/faces/2-image.jpg',
         Email : 'deepramani@gmail.com',
-        Status : "Inactive"
+        Status : "Inactive",
+        Access : "Manager"
     },
     {
         Name : "Deep Ramani",
         Image : 'https://reqres.in/img/faces/3-image.jpg',
         Email : 'deepramani@gmail.com',
-        Status : "Inactive"
+        Status : "Inactive",
+        Access : "Read"
     },
     {
         Name : "Deep Ramani",
         Image : 'https://reqres.in/img/faces/4-image.jpg',
         Email : 'deepramani@gmail.com',
-        Status : "Inactive"
+        Status : "Inactive",
+        Access : "Manager"
     },
     {
         Name : "Deep Ramani",
         Image : 'https://reqres.in/img/faces/5-image.jpg',
         Email : 'deepramani@gmail.com',
-        Status : "Inactive"
+        Status : "Inactive",
+        Access : "Read"
     },
     {
         Name : "Deep Ramani",
         Image : 'https://reqres.in/img/faces/6-image.jpg',
         Email : 'deepramani@gmail.com',
-        Status : "Inactive"
+        Status : "Inactive",
+        Access : "Manager"
     },
     {
         Name : "Deep Ramani",
         Image : 'https://reqres.in/img/faces/7-image.jpg',
         Email : 'deepramani@gmail.com',
-        Status : "Inactive"
+        Status : "Inactive",
+        Access : "Read"
     },
     {
         Name : "Deep Ramani",
         Image : 'https://reqres.in/img/faces/8-image.jpg',
         Email : 'deepramani@gmail.com',
-        Status : "Inactive"
+        Status : "Inactive",
+        Access :"Manager"
     },
     {
         Name : "Deep Ramani",
         Image : 'https://reqres.in/img/faces/9-image.jpg',
         Email : 'deepramani@gmail.com',
-        Status : "Inactive"
+        Status : "Inactive",
+        Access : "Read"
     },
     {
         Name : "Deep Ramani",
         Image : 'https://reqres.in/img/faces/10-image.jpg',
         Email : 'deepramani@gmail.com',
-        Status : "Inactive"
+        Status : "Inactive",
+        Access : "Manager"
     },
     {
         Name : "Deep Ramani",
         Image : 'https://reqres.in/img/faces/11-image.jpg',
         Email : 'deepramani@gmail.com',
-        Status : "Inactive"
+        Status : "Inactive",
+        Access : "Read"
     },
   
 ];
