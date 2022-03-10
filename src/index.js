561f96032f7c436d6f915f76c35f029f5bb706e4
Deep-Ramani <deepramani8505@gmail.com>
2022-03-10 15:57:48 +0530

    Rearrangement of import File

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+// import 'https://unpkg.com/@popperjs/core@2'
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
@@ -5,9 +8,6 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import store from './redux/store/store';
 import { Provider } from 'react-redux';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
-import 'https://unpkg.com/@popperjs/core@2'
 
 ReactDOM.render(
   <React.StrictMode>
